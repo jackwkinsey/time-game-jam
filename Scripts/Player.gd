908b129ae332ab5465fd12234cbd3eea12ec1ea5
Jack Kinsey <jack@hexbitstudios.com>
2020-05-03 20:55:01 -0400

    feat: add collision checking with raycast

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,22 +7,28 @@ var last_position = Vector2();
 var target_position = Vector2();
 var move_direction = Vector2();
 
+onready var ray = $RayCast2D;
+
 func _ready():
 	position = position.snapped(Vector2(TILE_SIZE, TILE_SIZE));
 	last_position = position;
 	target_position = position;
 	
 func _process(delta):
-	# move the player based on move direction
-	position += move_direction * SPEED * delta;
-	
-	# if the player moved further than one space, snap to target position
-	if position.distance_to(last_position) >= TILE_SIZE - SPEED * delta:
-		position = target_position;
+	if ray.is_colliding():
+		position = last_position;
+		target_position = last_position;
+	else:
+		# move the player based on move direction
+		position += move_direction * SPEED * delta;
+		
+		# if the player moved further than one space, snap to target position
+		if position.distance_to(last_position) >= TILE_SIZE - SPEED * delta:
+			position = target_position;
 	
 	if position == target_position:
 		# only get movement direction from input when player is at target position
-		move_direction = get_move_direction();
+		get_move_direction();
 		last_position = position;
 		target_position += move_direction * TILE_SIZE;
 	
@@ -31,12 +37,13 @@ func get_move_direction():
 	var left = int(Input.is_action_pressed("ui_left"));
 	var down = int(Input.is_action_pressed("ui_down"));
 	var up = int(Input.is_action_pressed("ui_up"));
-	var x = right - left;
-	var y = down - up;
+	
+	move_direction.x = right - left;
+	move_direction.y = down - up;
 	
 	# Prevent diagonal movement...eventually want to allow this!
-	if x != 0 && y != 0:
-		x = 0;
-		y = 0;
-	
-	return Vector2(x, y);
+	if move_direction.x != 0 && move_direction.y != 0:
+		move_direction = Vector2.ZERO;
+		
+	if move_direction != Vector2.ZERO:
+		ray.cast_to = move_direction * TILE_SIZE / 2;
